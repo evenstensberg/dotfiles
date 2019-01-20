9691034f7ac1c8dd8775f76d5247a2bb3c33dbaf
ev1stensberg <evenstensberg@gmail.com>
2019-01-20 08:01:14 +0100

    chore: dump hex

--- hexdumpi.sh
+++ hexdumpi.sh
@@ -4,6 +4,8 @@
 # ./hexdumpi.sh 0x07c00 0x07e00 add = add
 # ./hexdumpi.sh 0x07c00 0x07e00 sub = subtract
 # ./hexdumpi.sh 0x07e00 single = single arg
+# ./hexdumpi.sh 0x07c00<selector> 0x07e00<offset> lin = linear address
+# ./hexdumpi.sh 0x07c00<selector> 0x07e00<offset> rev = get offset
 
 # link globally: 
 # sudo ln -s pwd + /hexdumpi.sh /usr/local/bin/dumphexi
@@ -18,4 +20,22 @@ then
 elif [ "$2" == "single" ]; 
 then
     echo $(($1))
+elif [ "$3" == "lin" ]; 
+then
+    val=$(( ($1 << 4) + $2))
+    if [ "$4" == "hex" ];
+    then
+        printf '%x\n' $val
+    else
+        echo $val
+    fi
+elif [ "$3" == "rev" ]; 
+then
+   val=$(( $1 - ($2 << 4)))
+    if [ "$4" == "hex" ];
+    then
+        printf '%x\n' $val
+    else
+        echo $val
+    fi
 fi
