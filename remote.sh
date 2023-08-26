d5df21704460fe292a0337a0aaf6ed778e48ff00
Even Stensberg <evenstensberg@gmail.com>
2023-08-26 16:13:34 +0200

    feat: new script

---
+++ remote.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env sh
+
+# git remote add git remote add upstream git@git.assembla.com:portfolio/space.space_name.git
+
+git remote add $1 $2
+
+# push
+
+# git push upstream main --force
